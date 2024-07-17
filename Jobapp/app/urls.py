134bc31b1e32fbc1b40e232f6c39bd0946ceaa1b
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-07-17 17:07:39 +0530

    Getting data from the database-inapp

--- Jobapp/app/urls.py
+++ Jobapp/app/urls.py
@@ -3,6 +3,9 @@ from app import views
 
 
 urlpatterns = [
-    path("", views.hello),
-    path("job/<id>", views.job_detail),
+    path('', views.job_list, name='jobs_home'),
+    path('hello/', views.hello, name='hello'),
+    path('job/<int:id>', views.job_detail, name='jobs_detail'),
+    # path('job/<str:id>', views.job_detail),
+    # path('job/1', views.job_detail),
 ]
